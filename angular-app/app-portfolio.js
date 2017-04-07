86be85a21bfe98fdd1fb096051f456ac21527ade
Daniel Frank <danjfrank08@gmail.com>
2017-04-07 15:00:22 -0500

    Read changelog.txt

--- angular-app/app-portfolio.js
+++ angular-app/app-portfolio.js
@@ -1,49 +1,52 @@
 (function() {
     'use strict';
     angular
-        .module('portfolio', ['ui.router', 'ngTouch','ngAnimate', 'ngDialog', 'smoothScroll'])
-        .config(function($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise('/home');
-            $stateProvider
-                .state('home', {
-                    url: '/home',
-                    title: "Daniel Frank- Web Developer",
-                    templateUrl: 'angular-app/site-templates/home.html'
-                })
-                .state('about', {
-                    url: '/about',
-                    title: "About Me",
-                    templateUrl: 'angular-app/site-templates/about-me-proj.html'
-                })
-                .state('ryu', {
-                    url: '/ryu',
-                    title: "Ryu jQuery Page",
-                    templateUrl: 'angular-app/site-templates/ryu-proj.html'
-                })
-                .state('shop', {
-                    url: '/shop',
-                    title: "Shopping List App",
-                    templateUrl: 'angular-app/site-templates/shop-proj.html'
-                })
-                .state('hotcold', {
-                    url: '/hotcold',
-                    title: "Hot or Cold App",
-                    templateUrl: 'angular-app/site-templates/hotcold-proj.html'
-                })
-                .state('amelia-artist', {
-                    url: '/amelia-artist',
-                    title: "Amelia | Artist",
-                    templateUrl: 'angular-app/site-templates/amelia-artist.html',
-                    controller: 'AmeliaController'
-                })
-                .state('am-port', {
-                    url: '/am-port:id',
-                    title: "Amelia | Artist - Portfolio",
-                    templateUrl: 'angular-app/site-templates/am-portfolio.html',
-                    controller: 'AmeliaController',
-                    params: {id: "0"}
-                })
-        })
+    .module('portfolio', ['ui.router', 'ngTouch','ngAnimate', 'ngDialog', 'smoothScroll'])
+    .run([ '$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
+    }])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'angular-app/site-templates/home.html',
+                params: {title: "Daniel Frank | Web Developer"}
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'angular-app/site-templates/about-me-proj.html',
+                params: {title: "Daniel Frank | About Me"}
+            })
+            .state('ryu', {
+                url: '/ryu',
+                templateUrl: 'angular-app/site-templates/ryu-proj.html',
+                params: {title: "Daniel Frank | Ryu jQuery Project"}
+            })
+            .state('shop', {
+                url: '/shop',
+                templateUrl: 'angular-app/site-templates/shop-proj.html',
+                params: {title: "Daniel Frank | Shopping List App"}
+            })
+            .state('hotcold', {
+                url: '/hotcold',
+                templateUrl: 'angular-app/site-templates/hotcold-proj.html',
+                params: {title: "Daniel Frank | Hot or Cold Game"}
+            })
+            .state('amelia-artist', {
+                url: '/amelia-artist',
+                templateUrl: 'angular-app/site-templates/amelia-artist.html',
+                controller: 'AmeliaController',
+                params: {title: "Amelia | Artist"}
+            })
+            .state('am-port', {
+                url: '/am-port:id',
+                templateUrl: 'angular-app/site-templates/am-portfolio.html',
+                controller: 'AmeliaController',
+                params: {id: "0", title: "Amelia | Artist - Portfolio"}
+            })
+    })
 
     // Attirbute Directives
     .directive('nav', function() {
