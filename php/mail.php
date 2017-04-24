25fd833bcc9a16efc39c22503e723d94fa419318
Daniel Frank <danjfrank08@gmail.com>
2017-04-24 15:28:59 -0500

    Updated mail function

--- php/mail.php
+++ php/mail.php
@@ -1,30 +1,29 @@
 <?php
-    $response = array( 'success' => false );
-    $formData = file_get_contents( 'php://input' );
-    $data = json_decode($formData);
-    #if ( $data->submit ) {
-        $name = $data->name;
-        $email = $data->email;
-        $phone = $data->phone;
-        $service = $data->service;
-        $message = $data->message;
+$response = array( 'success' => false );
+$formData = file_get_contents( 'php://input' );
+$data = json_decode($formData);
 
-        #if ( $name != '' && $email != '') {
-            $mailTo = 'danjfrank08@gmail.com';
-            $subject = 'New website form submission';
-            $body  = 'From: ' . $name . "\n";
-            $body .= 'Email: ' . $email . "\n";
-            $body .= 'Phone: ' . $phone . "\n";
-            $body .= 'Interested in: ' . $service . "\n";
-            $body .= 'Message: ' . "\n" . $message;
+$name = $data->name;
+$email = $data->email;
+$phone = $data->phone;
+$service = $data->service;
+$message = $data->message;
+$mailTo = 'danjfrank08@gmail.com';
+$subject = 'New website form submission';
+$headers = 'From: ' . $email . "\r\n".
+'Reply-To: ' . $email . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+$body  = 'From: ' . $name . "\n" .
+'Email: ' . $email . "\n" .
+'Phone: ' . $phone . "\n" .
+'Interested in: ' . $service . "\n" .
+'Message: ' . "\n" . $message;
 
-            $success = mail( $mailTo, $subject, $body );
-            mail( $mailTo, $subject, $body );
-            if ( $success ) {
-                $response[ 'success' ] = true;
-            }
-        #}
-    #}
+$success = mail( $mailTo, $subject, $body, $headers );
+@mail( $mailTo, $subject, $body, $headers );
+if ( $success ) {
+    $response[ 'success' ] = true;
+}
 
-    echo json_encode( $response );
+echo json_encode( $response );
 ?>
