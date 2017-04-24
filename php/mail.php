dd02424231215e156b55ba17ef8d29a66eed8e01
Daniel Frank <danjfrank08@gmail.com>
2017-04-24 15:48:42 -0500

    Updated mail function

--- php/mail.php
+++ php/mail.php
@@ -4,14 +4,14 @@ $formData = file_get_contents( 'php://input' );
 $data = json_decode($formData);
 
 $name = $data->name;
-$email = $data->email;
+$emailaddy = $data->email;
 $phone = $data->phone;
 $service = $data->service;
 $message = $data->message;
 $mailTo = 'danjfrank08@gmail.com';
 $subject = 'New website form submission';
-$headers = 'From: ' . $email . "\r\n".
-'Reply-To: ' . $email . "\r\n" .
+$headers = 'From: ' . $emailaddy . "\r\n".
+'Reply-To: ' . $emailaddy . "\r\n" .
 'X-Mailer: PHP/' . phpversion();
 $body  = 'From: ' . $name . "\n" .
 'Email: ' . $email . "\n" .
@@ -20,7 +20,7 @@ $body  = 'From: ' . $name . "\n" .
 'Message: ' . "\n" . $message;
 
 $success = mail( $mailTo, $subject, $body, $headers );
-@mail( $mailTo, $subject, $body, $headers );
+mail( $mailTo, $subject, $body, $headers );
 if ( $success ) {
     $response[ 'success' ] = true;
 }
