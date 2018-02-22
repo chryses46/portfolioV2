ad330fa69ecaa162fdaa09d4d6fb489f771130ab
Daniel Frank <danjfrank08@gmail.com>
2018-02-22 11:49:29 -0600

    Updates

--- angular-app/controllers/blog.controller.js
+++ angular-app/controllers/blog.controller.js
@@ -3,7 +3,7 @@ angular.module('portfolio')
 
       $scope.BlogJournal = [
         {title: 'Wecome to my blog!',
-        entry: "Since I have never written a blog before, you'll have to bear with me. Most people think that web development is easy, or that using Word Press or Wix is a better solution. While using these web-site builders helps, it also vastly limits your potential for unique and beautiful web design and development. Most people want to upload pictures to their site as well, but most of these builders have limits on their storage. When deciding to use custom hosting and site design, you generally have vastly more, or infinate, storage for your site's content.",
+        entry: "Since I have never written a blog before, you'll have to bear with me. Most people think that web development is easy, or that using Word Press or Wix is a better solution. While using these web-site builders helps, it also vastly limits your potential for unique and beautiful web design and development. Most people want to upload pictures to their site as well, but most of these builders have limits on their storage. When deciding to use custom hosting and site design, you generally have vastly more, or infinite, storage for your site's content.",
         index: 0,
         img: '/img/dallas-seo-blog/1.jpg'}/*,
         {title: 'My second blog post!',
